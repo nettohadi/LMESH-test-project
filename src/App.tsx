0d2573c14884d24092bc42c4c68b1e30b5303e89
nettohadi <nettohadi@users.noreply.github.com>
2026-06-26 20:45:25 +0800

    feat: Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      <section id="center" className="bg-purple-400">
-        
-      </section>
+      <main className="bg-stone-200 w-full h-full flex justify-center pt-24 p-20">
+        <div className="bg-white rounded-lg w-full h-full shadow-md p-5">
+          <h1 className="text-2xl font-bold">BIANCHINI Thierry</h1>
+          <h3 className="text-stone-600">Workload</h3>
+          <div></div>
+        </div>
+      </main>
     </>
   )
 }
